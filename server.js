6e226a6720dd97373b37c49498e0f41b9d50b599
Ronaldo <ronaldomorales.ai@gmail.com>
2026-05-26 16:58:43 -0400

    Updated server.js

--- server.js
+++ server.js
@@ -3,19 +3,19 @@ const app = express();
 const mongoose = require("mongoose");
 const passport = require("passport");
 const session = require("express-session");
-const MongoStore = require("connect-mongo")(session);
+const MongoStore = require("connect-mongo").default
 const methodOverride = require("method-override");
 const flash = require("express-flash");
 const logger = require("morgan");
-const connectDB = require("./config/database");
+const connectDB = require("./configFile/database");
 const mainRoutes = require("./routes/main");
 const postRoutes = require("./routes/posts");
 
 //Use .env file in config folder
-require("dotenv").config({ path: "./config/.env" });
+require("dotenv").config({ path: "./configFile/.env" });
 
 // Passport config
-require("./config/passport")(passport);
+require("./configFile/passport")(passport);
 
 //Connect To Database
 connectDB();
@@ -42,7 +42,7 @@ app.use(
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: false,
-    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    store: MongoStore.create({ mongoUrl: process.env.DB_STRING })
   })
 );
 
